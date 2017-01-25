68f773a9739e765dbab15ee6f6f4d045ee636af0
masoudmasoud <msimasoud@gmail.com>
2017-01-25 13:18:18 +0330

    2commit

--- web/models.py
+++ web/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    question_text = models.CharField(max_length=200)
+    stu_name = models.CharField(max_length=200)
