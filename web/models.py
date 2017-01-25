3d5a13d8f76d42ea16fc1de586feab25e089f947
masoudmasoud <msimasoud@gmail.com>
2017-01-25 13:35:57 +0330

    3commit

--- web/models.py
+++ web/models.py
@@ -5,3 +5,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     stu_name = models.CharField(max_length=200)
+    stu_family= models.CharField(max_length=200)
+    stu_password= models.CharField(max_length=200)
+
+    stu_phone= models.CharField(max_length=200)
