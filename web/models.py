783c2ddef9ea207987dffe36b02543bd251f7650
masoudmasoud <msimasoud@gmail.com>
2017-01-25 13:16:24 +0330

    2commit

--- web/models.py
+++ web/models.py
@@ -3,3 +3,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    question_text = models.CharField(max_length=200)
